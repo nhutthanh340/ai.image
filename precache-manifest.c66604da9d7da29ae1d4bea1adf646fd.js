9ec3435cb3065a6ede10a78640670e6e9a02ccaa
nhutthanh340 <nhutthanh340@gmail.com>
2020-04-24 22:35:31 +0700

    first commit

---
+++ precache-manifest.c66604da9d7da29ae1d4bea1adf646fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2981dad63a0b7ffbe44bcb3efa29e399",
+    "url": "/ai-image.github.io/index.html"
+  },
+  {
+    "revision": "7a0c16dcc2b0d30917cb",
+    "url": "/ai-image.github.io/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "3bb4f51111e685ae463b",
+    "url": "/ai-image.github.io/static/css/main.57f953d6.chunk.css"
+  },
+  {
+    "revision": "7a0c16dcc2b0d30917cb",
+    "url": "/ai-image.github.io/static/js/2.27f4a5f9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ai-image.github.io/static/js/2.27f4a5f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bb4f51111e685ae463b",
+    "url": "/ai-image.github.io/static/js/main.247b05a3.chunk.js"
+  },
+  {
+    "revision": "9b461928cd24b7d87c72",
+    "url": "/ai-image.github.io/static/js/runtime-main.8d3b8b3e.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/ai-image.github.io/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
